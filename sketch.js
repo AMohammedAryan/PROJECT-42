94ff2d83efaca0164cef9ffaf69c442c0773ae10
AMohammedAryan <69350797+AMohammedAryan@users.noreply.github.com>
2020-11-05 18:48:31 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,14 +7,14 @@ angleMode(DEGREES);
 function setup() {
   createCanvas(800,400);
   //createSprite(400, 200, 50, 50);
+
+  scAngle = map(sc, 0, 60, 0, 360);
+  minAngle = map(min, 0, 60, 0, 360);
+  hrAngle = map(hr%12, 0, 24, 0, 360);
 }
 
 function draw() {
   background(255,255,255);  
-
-  scAngle = map(sc, 0, 60, 0, 360);
-  minAngle = map(min, 0, 60, 0, 360);
-  hrAngle = map(hr, 0, 12 % 24, 0, 360);
 
   translate(width/2, height/2);
 
@@ -23,7 +23,7 @@ function draw() {
   rotate(scAngle);
   stroke("red");
   strokeWeight(7);
-  line(0, 0, 100, 0);
+  line(0, 0, 300, 0);
 
   pop();
 
@@ -32,7 +32,7 @@ function draw() {
   rotate(minAngle);
   stroke("green");
   strokeWeight(7);
-  line(0, 0, 100, 0);
+  line(0, 0, 200, 0);
 
   pop();
 
@@ -45,7 +45,17 @@ function draw() {
 
   pop();
 
+  fill("red");
+
+  arc(0, 0, 300, 300, 0, scAngle);
+
+  fill("green");
+
+  arc(0, 0, 200, 200, 0, minAngle);
+
+  fill("blue");
+
+  arc(0, 0, 100, 100, 0, hrAngle);
+
   drawSprites();
 }
-
-console.log(hrAngle);
